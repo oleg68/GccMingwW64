0de365e19e7b26020368c1f2330a2abe35fc3e94
Oleg Samarin <osamarin@openintegration.inc>
2025-10-17 22:07:38 +0300

    Added patches

--- build-scripts/for-linux/build-on-ubuntu.bash
+++ build-scripts/for-linux/build-on-ubuntu.bash
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+# $1 - Patch source Dir. If not set then relative to the script dir
+
 set -e
+
+DIR=$(readlink -f $(dirname $0))
+
+if [[ -n "$1" ]]; then
+	SRC_DIR=$1
+else
+	SRC_DIR=$(readlink -f $(dirname $0)/../..)
+fi
 
 mkdir -p build/ubuntu
 pushd build/ubuntu
@@ -10,6 +20,8 @@ rm -rf gcc-mingw-w64-*
 dpkg-source -x gcc-mingw-w64*.dsc
 
 cd gcc-mingw-w64-*
+patch -p1 $SRC_DIR/patches/gcc-mingw-w64-26.1.patch
 debuild -us -uc
 
 popd
+
